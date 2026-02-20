6c00d3c93d8ec0170c2a8d12344af8bda5faebe2
jackb <jackb@WIN-TOH4A3L6TPC>
2026-02-20 22:24:14 +0800

    remove comment codes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
-using asp.net_core_8._0_web_api.Configurations;
 using asp.net_core_8._0_web_api.Middlewares;
 using asp.net_core_8._0_web_api.Services;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,25 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-// Configure MySQL database
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 // Register services
 builder.Services.AddScoped<IWeatherService, WeatherService>();
-//builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IDatabaseTestService, DatabaseTestService>();
 
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
 
 app.UseSwagger();
 app.UseSwaggerUI();
